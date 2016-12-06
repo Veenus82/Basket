298c1aba447ed7853d008a4bdad525c828022b04
veenus82 <markus.malm@gmail.com>
2016-12-06 19:03:25 +0200

    UnitTests
    original pythom program written in c# with added functionality and unit
    tested with nunit

---
+++ Basket/Program .cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Basket
+{
+    public class Program
+    {
+        public string Customer { get; set; }
+        public List<string> Contents { get; set; }
+        public double Price { get; set; }
+
+        public Program (string customer, List<string> contents, double price)
+        {
+            this.Customer = customer;
+            this.Contents = contents;
+            this.Price = price;
+        }
+
+        public string returnCustomer()
+        {
+            return this.Customer;
+        }
+
+        public List<string> returnContents()
+        {
+            return this.Contents;
+        }
+        public double returnPrice()
+        {
+            return this.Price;
+        }
+        public void addProduct(string product, double productPrice)
+        {
+            this.Contents.Add(product);
+            this.Price += productPrice;
+        }
+        public void deleteProduct (string product, double productPrice)
+        {
+            foreach (string item in this.Contents)
+            {
+                if (item == product)
+                {
+                    this.Contents.Remove(item);
+                    this.Price -= productPrice;
+                }
+            }
+        }
+
+        public double calculateDiscountPrice (double percent)
+        {
+            double discount = percent / 100;
+            return this.Price - this.Price * discount;
+        }
+
+        public double calculateAverageItemPrice()
+        {
+            return this.Price / this.Contents.Count;
+        }
+        static void Main(string[] args)
+        {
+        }
+    }
+}
---
+++ NUnit.Tests1/TestClass.cs
@@ -0,0 +1,74 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnitTests
+{
+    [TestFixture]
+    public class TestClass
+    {
+        List<string> items;
+        Basket.Program basket;
+        [SetUp]
+        public void SetUp()
+        {
+            items = new List<string>();
+            items.Add("omena");
+            items.Add("banaani");
+            basket = new Basket.Program("Jari", items, 25.0);
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            items = null;
+            basket = null;
+        }
+
+        [Test]
+        public void IsCustomerString()
+        {
+            Assert.That(basket.returnCustomer(), Is.InstanceOf(typeof(string)));
+        }
+
+        [Test]
+        public void IsPriceDouble()
+        {
+            Assert.That(basket.returnPrice(), Is.InstanceOf(typeof(double)));
+        }
+        [Test]
+        public void IsContentsList()
+        {
+            Assert.That(basket.returnContents(), Is.InstanceOf(typeof(List<string>)));
+        }
+
+        [Test]
+        public void AddToContents()
+        {
+            basket.addProduct("kala", 5.0);
+            Assert.That(basket.returnContents(), Contains.Item("kala"));
+        }
+
+        [Test]
+        public void DeleteFromList()
+        {
+            basket.deleteProduct("kala", 5.0);
+            Assert.That(basket.returnContents(), !Contains.Item("kala"));
+        }
+
+        [Test]
+        public void CalculateDiscount()
+        {
+            Assert.That(basket.calculateDiscountPrice(10), Is.EqualTo(22.5));
+        }
+
+        [Test]
+        public void CalculateAveragePrice()
+        {
+            Assert.That(basket.calculateAverageItemPrice(), Is.EqualTo(12.5));
+        }
+    }
+}
